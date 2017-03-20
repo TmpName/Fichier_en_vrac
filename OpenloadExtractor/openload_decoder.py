7ff6776fdb5e3f824dec50ab3a45ac4c02217b4e
TmpName <gihubtmp@yopmail.com>
2017-03-20 19:21:47 +0100

    Update openload_decoder.py

--- OpenloadExtractor/openload_decoder.py
+++ OpenloadExtractor/openload_decoder.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*- 
 
+# Attention dans ce cas precis Le fichier html est en utf-16
+# Les chemins d'acces sont a modifier aussi
+
 from aadecode import AADecoder
 from jjdecode import JJDecoder
 from packer import cPacker
 
 import re
-
 
 Coded_url = 'abcdefghijkl'
 
@@ -48,7 +50,7 @@ def CheckCpacker(str):
             str2 = str2 + ';'
         try:
             str = cPacker().unpack(str2)
-            #xbmc.log('Cpacker encryption')
+            print('Cpacker encryption')
         except:
             pass
 
@@ -59,18 +61,16 @@ def CheckJJDecoder(str):
     sPattern = '([a-z]=.+?\(\)\)\(\);)'
     aResult = re.findall(sPattern,str)
     if (aResult):
-        #xbmc.log('JJ encryption')
+        print('JJ encryption')
         return JJDecoder(aResult[0]).decode()
         
     return str
     
 def CheckAADecoder(str):
-    sPattern = '([>;]\s*)(ﾟωﾟ.+?\(\'_\'\);)'
-    aResult = re.search(sPattern, str,re.DOTALL | re.UNICODE)
+    aResult = re.search('([>;]\s*)(ﾟωﾟ.+?\(\'_\'\);)', str,re.DOTALL | re.UNICODE)
     if (aResult):
-        #xbmc.log('AA encryption')
+        print('AA encryption')
         tmp = aResult.group(1) + AADecoder(aResult.group(2)).decode()
-        #xbmc.log('>> ' + tmp)
         return str[:aResult.start()] + tmp + str[aResult.end():]
         
     return str
@@ -82,6 +82,10 @@ fh = open('G:\\openload\\html.txt', "r")
 sHtmlContent1 = fh.read().decode('utf-16','replace')
 fh.close()
 
+#verif
+#print sHtmlContent1[:100]
+sHtmlContent1 = sHtmlContent1.encode('utf-8')
+print isinstance(sHtmlContent1, unicode)
 
 #import codecs
 #f = codecs.open("G:\\openload\\html.txt", "rb", "utf-8")
@@ -94,8 +98,9 @@ TabUrl = []
 sPattern = r'<span id="([^"]+)">([^<>]+)<\/span>'
 aResult = re.findall(sPattern,sHtmlContent1)
 if (aResult):
-    print 'Key : ' + aResult[0][1]
-    TabUrl = aResult[0][1]
+    TabUrl = aResult
+    Coded_url = aResult[0][1]
+    print 'key : ' + Coded_url
 else:
     print 'er1'
     
@@ -105,11 +110,12 @@ sPattern = '<script src="\/assets\/js\/video-js\/video\.js\.ol\.js"(.+)*'
 aResult = re.findall(sPattern,sHtmlContent1, re.DOTALL)
 if (aResult):
     sHtmlContent3 = aResult[0]
+    print 'code trouve'
 else:
     print 'er2'
     
     
-maxboucle = 5
+maxboucle = 4
 while (maxboucle > 0):
     sHtmlContent3 = CheckCpacker(sHtmlContent3)
     #xbmc.log(sHtmlContent3)
@@ -131,25 +137,26 @@ if r:
 
 #1 er decodage
 code = ASCIIDecode(code)
-    
+
 #extract first part
-r = re.search(r'\[\'ready\'\]\(function\(\){(.+?)}\);\$\("#videooverlay', code,re.DOTALL)
+P3 = "\['ready'\]\(function\(\){(.+?)}\);\s*\$\("#videooverlay"
+r = re.search(P3, code,re.DOTALL)
 if r:
     code = r.group(1)
+else:
+    print 'rt4'
 
 # 1 pass
 r = re.search(r'var (_0x[0-9a-z]+)={(.+?)}};', code,re.DOTALL)
 if r:
+    #on supprime cette partie
     code = code[:r.start()] + code[r.end():]
+    
     name1 = r.group(1)
     pass1 = r.group(2)
     # Name, param , return
     tab1 = re.findall(r'\'([a-zA-Z]+)\':function _0x[0-9a-z]+\((.+?)\){return (.+?);}', pass1,re.DOTALL)
-    
-    #rewrite code
-    for n,p,r in tab1:
-        code = 'function '+ n + '(' + p + ') { return ' + r + '; }\n' + code
-
+        
 #replace code from 1 pass
 for n,p,r in tab1:
     code = code.replace(name1 + "['" + n + "']" , n)
@@ -160,16 +167,32 @@ code = re.sub("\['([^']+)'\]",'.\\1',code)
 code = re.sub('([^_])(0x[0-9a-f]+)',SubHexa,code)
 
 #insert coded url
-code = re.sub("[a-zA-Z]+\(\$,[a-zA-Z]+\('#',r\)\)\.text\(\)","'" + Coded_url + "'",code)
+P1 = "[a-zA-Z]+\(\$,[0-9_a-zA-Z\.]+\('#',r\)\)\.text\(\)"
+code = re.sub(P1,"'" + Coded_url + "'",code)
+#insert code tu get result
+P5 = "[a-zA-Z]+\(\$,'#streamurl'\)\.text\((_0x5e5aae.join\(''\))\)"
+code = re.sub(P5,'Result=\\1',code)
  
 #Saut de ligne
 #code = code.replace(';',';\n')
+code = code.replace('case','\ncase')
+code = code.replace('}','\n}\n')
+code = code.replace('{','{\n')
+
+#tab
+code = code.replace('\t','')
 
 #hack
 code = code.replace('!![]','true')
 
+#rewrite code
+#1 pass
+for n,p,r in tab1:
+    code = 'function '+ n + '(' + p + ') { return ' + r + '; }\n' + code
+
 #save file
-code = code.encode('utf-16')
-fh = open('G:\\openload\\output.txt', "w")
+#code = code.encode('utf-8')
+fh = open('G:\\OpenloadExtractor\\output.txt', "w")
 fh.write(code)
 fh.close()
+print "File cleaned"
